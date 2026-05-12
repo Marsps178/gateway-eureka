67d9f4feeda329122f51d6bce90f10a987b7ee47
unknown <SUITE@SUITEBG302-ST18.cibertec.edu.pe>
2026-05-12 16:43:38 -0500

    Add to server config

--- src/main/java/cibertec/pe/EurekaServerApplication.java
+++ src/main/java/cibertec/pe/EurekaServerApplication.java
@@ -2,12 +2,15 @@ package cibertec.pe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+		System.out.println("Proyecto Eureka Inicializado");
 	}
 
 }
